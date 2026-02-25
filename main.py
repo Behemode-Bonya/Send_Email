d74047bfd443975c29dd105a50aeb9ce50b6759a
Behemode-Bonya <karinakaa1966@gmail.com>
2026-02-25 18:50:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,20 +5,17 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-TOKEN = os.getenv("API")
+TOKEN = os.getenv("TOKEN")
 
 server = smtplib.SMTP_SSL('smtp.gmail.com:465')
 
 email = "karinakaa1966@gmail.com"
-password = TOKEN
-server.login(email, password)
-
+server.login(email, password=TOKEN)
 feature_lessons =  ["Github", "API", "JS"]
 finish_lessons = ["Введение Python", "Командная строка", "Веб-разработка"]
 monthes = "6 месяцев"
-email_from =  email
 email_to = 'karinakaa1966@gmail.com'
 subject = 'Варр приде к вам'
 text = f'Привет Мама(Папа), я занимаюсь в школе третье место уже {monthes}. В процессе я выполнил модули: {finish_lessons}! Сейчас я работаю над модулями {feature_lessons}. Обучение мне нравится, я получил море знаний!'
 
-server.sendmail(email_from, email_to, f"Subject: {subject}\n\n{text}")
+server.sendmail(email_from, email, f"Subject: {subject}\n\n{text}")
