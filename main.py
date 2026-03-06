55cab49bf8df6481431b1a4f9661fbb899c5b7f5
Behemode-Bonya <karinakaa1966@gmail.com>
2026-03-06 19:04:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,21 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-TOKEN = os.getenv("TOKEN")
+PASSWORD = os.getenv("PASSWORD")
 
 server = smtplib.SMTP_SSL('smtp.gmail.com:465')
 
 email = "karinakaa1966@gmail.com"
-server.login(email, password=TOKEN)
+server.login(email, password=PASSWORD)
 feature_lessons =  ["Github", "API", "JS"]
 finish_lessons = ["Введение Python", "Командная строка", "Веб-разработка"]
 monthes = "6 месяцев"
 email_to = 'karinakaa1966@gmail.com'
 subject = 'Варр приде к вам'
 text = f'Привет Мама(Папа), я занимаюсь в школе третье место уже {monthes}. В процессе я выполнил модули: {finish_lessons}! Сейчас я работаю над модулями {feature_lessons}. Обучение мне нравится, я получил море знаний!'
-alternative_text = f"Привет Мама(Папа), я занимаюсь в школе третье место уже {monthes}. Сейчас я работаю над модулями {finish_lessons}. Пока что я улучшаю свои навыки и узнаю много нового!"
-
-server.sendmail(email_from, email, f"Subject: {subject}\n\n{text}")
+if finish_lessons:
+    server.sendmail(email_from, email, f"Subject: {subject}\n\n{text}")
+else:
+    alternative_text = f"Привет Мама(Папа), я занимаюсь в школе третье место уже {monthes}. Сейчас я работаю над модулями {finish_lessons}. Пока что я улучшаю свои навыки и узнаю много нового!"
+    server.sendmail(email_from, email, f"Subject: {subject}\n\n{alternative_text}")
+  
